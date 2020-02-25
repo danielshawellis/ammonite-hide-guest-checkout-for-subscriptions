fdf30eab3870aaad135981884a3a64810f15aeb3
Daniel Ellis <danielshawellis@gmail.com>
2020-02-25 15:32:48 -0600

    add plugin header and basic meta

---
+++ ammonite-hide-guest-checkout-for-subscriptions.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name:       Hide Guest Checkout Option For Subscriptions
+ * Description:       This plugin runs a script on the checkout page that checks wheather a subscription is in the cart and dynamically hides the "Checkout As Guest" button with CSS for subscription orders.
+ * Version:           1.0.0
+ * Author:            Daniel Ellis
+ * Author URI:        https://danielellisdevelopment.com/
+ */
